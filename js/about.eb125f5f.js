d0ac17d9b2108c620a97fbf60f0a3ea6f24b5398
zt <2624638269@qq.com>
2022-10-11 21:30:46 +0800

    first commit

---
+++ js/about.eb125f5f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwyy_app"]=self["webpackChunkwyy_app"]||[]).push([[443],{5569:function(n,a,u){u.r(a),u.d(a,{default:function(){return f}});var t=u(3396);const e={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,t.wg)(),(0,t.iD)("div",e,s)}var i=u(89);const p={},o=(0,i.Z)(p,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.eb125f5f.js.map
